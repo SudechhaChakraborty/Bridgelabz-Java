ff308a40a3d618a2c1c4a39ddad12909e540bc1b
Sudechha Chakraborty <143380382+SudechhaChakraborty@users.noreply.github.com>
2026-01-23 11:22:41 +0530

    [Sudechha] : [Update] : InsertionSortEmployeeIDs.java

--- src/datastructure/sortingalgorithms/InsertionSortEmployeeIDs.java
+++ src/datastructure/sortingalgorithms/InsertionSortEmployeeIDs.java
@@ -1,4 +1,26 @@
 package datastructure.sortingalgorithms;
 
-public class InsertionSortEmployeeIDs {
+class InsertionSortEmployee {
+    public static void main(String[] args) {
+
+        int[] empIds = {104, 101, 105, 102, 103};
+
+        for (int i = 1; i < empIds.length; i++) {
+            int key = empIds[i];
+            int j = i - 1;
+
+            while (j >= 0 && empIds[j] > key) {
+                empIds[j + 1] = empIds[j];
+                j--;
+            }
+
+            empIds[j + 1] = key;
+        }
+
+        // Display sorted employee IDs
+        System.out.println("Sorted Employee IDs:");
+        for (int id : empIds) {
+            System.out.print(id + " ");
+        }
+    }
 }
